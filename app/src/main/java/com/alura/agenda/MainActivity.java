057a70824cd4f4c0420e3f00c764ca481eea2655
Marcos Luciano <marcos3dt@gmail.com>
2021-04-23 23:50:38 -0300

    colocar comportamentos a mais na nossa Activity

--- app/src/main/java/com/alura/agenda/MainActivity.java
+++ app/src/main/java/com/alura/agenda/MainActivity.java
@@ -2,7 +2,6 @@ package com.alura.agenda;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -10,6 +9,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toast.makeText(this, "Marcos Luciano", Toast.LENGTH_SHORT).show();
+        setContentView(R.layout.activity_main);
     }
+
 }
