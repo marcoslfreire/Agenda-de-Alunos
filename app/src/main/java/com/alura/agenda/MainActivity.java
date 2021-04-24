7e959493a8d8e6846ababf8282eae36e14c27998
Marcos Luciano <marcos3dt@gmail.com>
2021-04-23 22:44:02 -0300

    criando layout activity_main

--- app/src/main/java/com/alura/agenda/MainActivity.java
+++ app/src/main/java/com/alura/agenda/MainActivity.java
@@ -1,6 +1,15 @@
 package com.alura.agenda;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toast.makeText(this, "Marcos Luciano", Toast.LENGTH_SHORT).show();
+    }
 }
