bc9b98bd9185e872ea690ff69ff7b14875253840
Marcos Luciano <marcos3dt@gmail.com>
2021-04-24 23:27:58 -0300

    Activity de formulario comcluida

--- app/src/main/java/com/alura/agenda/ui/activity/FormularioAlunoActivity.java
+++ app/src/main/java/com/alura/agenda/ui/activity/FormularioAlunoActivity.java
@@ -3,8 +3,14 @@ package com.alura.agenda.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.alura.agenda.R;
+import com.alura.agenda.model.Aluno;
 
 public class FormularioAlunoActivity extends AppCompatActivity {
 
@@ -12,5 +18,27 @@ public class FormularioAlunoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_aluno);
+
+        final EditText campoNome = findViewById(R.id.activity_formulario_aluno_nome);
+        final EditText campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
+        final TextView campoEmail = findViewById(R.id.activity_formulario_aluno_emael);
+
+        Button bottonSalvar = findViewById(R.id.activity_formulario_aluno_botton);
+        bottonSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nome = campoNome.getText().toString();
+                String telefone = campoTelefone.getText().toString();
+                String email = campoEmail.getText().toString();
+
+                Aluno alunoCriado = new Aluno(nome,telefone,email);
+                Toast.makeText(FormularioAlunoActivity.this,
+                        alunoCriado.getNome() + " - "
+                                + alunoCriado.getTelefone()
+                                + " - " + alunoCriado.getEmail(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
